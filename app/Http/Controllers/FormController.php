58fc6dccceaf04f03a08b0c67afd1828a2748657
VanessaSimanta <vanessasimanta@gmail.com>
2024-06-18 19:56:28 +0700

    remove service create models

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\FormService;
+
+class FormController extends Controller
+{
+    function addForm(Request $req){
+        
+    }
+}
