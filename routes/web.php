4bea557708a25f4e602112417ee414f5a64cb1b2
VanessaSimanta <vanessasimanta@gmail.com>
2024-06-22 09:59:25 +0700

    create post form

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DataController;
+use App\Http\Controllers\FormController;
+
 
 Route::controller(dataController::class)->group(function () {
     Route::get('/', 'index');
 });
+
+Route::post('/', [FormController::class, 'addForm'])->name('form.message');
